2c862660b53b436618062ad0f19aaecd60c0adbf
lhx-01 <2182794453@qq.com>
2020-10-11 12:49:39 +0800

    查询

--- StuSystem/src/main/java/com/kgc/springssm/controller/StandardController.java
+++ StuSystem/src/main/java/com/kgc/springssm/controller/StandardController.java
@@ -1,17 +1,23 @@
 package com.kgc.springssm.controller;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.kgc.springssm.pojo.Standard;
+import com.kgc.springssm.pojo.StandardExample;
 import com.kgc.springssm.service.StandardService;
-import jdk.nashorn.internal.ir.ReturnNode;
 import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.lang.math.RandomUtils;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
+import javax.security.auth.login.CredentialException;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.io.File;
+import java.util.List;
 
 /**
  * @author shkstart
@@ -22,8 +28,28 @@ public class StandardController {
     @Resource
     StandardService standardService;
     @RequestMapping("/")
-    public String toindex(){
-        return "index";
+    public String toindex(Model model, String pageIndex,String title){
+        int pageNum=1;
+        if(pageIndex!=null){
+            pageNum=Integer.parseInt(pageIndex);
+        }
+        int pageSize=3;
+        PageHelper.startPage(pageNum,pageSize);
+        if(title != null && title.isEmpty() == false){
+            StandardExample example=new StandardExample();
+            StandardExample.Criteria criteria = example.createCriteria();
+            criteria.andZhnameLike("%"+title+"%");
+            List<Standard> standards = standardService.selectByExample(example);
+            PageInfo pageInfo=new PageInfo(standards);
+            model.addAttribute("pageInfo",pageInfo);
+            return "index";
+        }else{
+            PageHelper.startPage(pageNum,pageSize);
+            List<Standard> standards = standardService.selectByExample(null);
+            PageInfo pageInfo=new PageInfo(standards);
+            model.addAttribute("pageInfo",pageInfo);
+            return "index";
+        }
     }
     @RequestMapping("/toadd")
     public  String toadd(){
